eada34c797cfcc79bb282bd922ccc8c07f1de2c2
josevissoti <josevissoti@gmail.com>
2025-03-05 10:42:43 -0300

    Regex e Scanner

--- src/academy/devdojo/maratonajava/javacore/Uregex/view/PatterMatcherView03.java
+++ src/academy/devdojo/maratonajava/javacore/Uregex/view/PatterMatcherView03.java
@@ -3,7 +3,7 @@ package academy.devdojo.maratonajava.javacore.Uregex.view;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatterMatcherView02 {
+public class PatterMatcherView03 {
     public static void main(String[] args) {
         // \d = Todos os digitos
         // \D = Tudo que não for digito
@@ -12,29 +12,20 @@ public class PatterMatcherView02 {
         // \w = a-Z A-Z, dígitos, _
         // \W = Tudo o que não fot incluso no \w
         // []
-        String regex = "[a-zA-C]";
-        String texto = "cafeBASE";
+        // ? zero ou uma
+        // * zero ou mais
+        // + uma ou mais
+        // {n, m} de n até m
+        // () agrupamento
+        // | ou
+        // $ fim da linha
+        String regex = "0[xX]([0-9a-fA-F])+(\\s|$)";
+        String texto = "12 0x 0x 0xFFABC 0x10G 0x1";
+
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto);
 
         System.out.println("\nTexto:  " + texto);
-        System.out.println("Índice: 0123456789");
-        System.out.println("regex: " + regex);
-        System.out.println("Posições encontradas");
-        while (matcher.find()) {
-            System.out.print(matcher.start() + " " + matcher.group() + "\n");
-        }
-
-        int numeroHex = 0x59F86A;
-        System.out.println(numeroHex);
-
-        regex = "0[xX][0-9a-fA-F]";
-        String texto2 = "12 0x 0x 0xFFABC 0x109 0x1";
-
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(texto2);
-
-        System.out.println("\nTexto:  " + texto2);
         System.out.println("Índice: 0123456789");
         System.out.println("regex: " + regex);
         System.out.println("Posições encontradas");
--- src/academy/devdojo/maratonajava/javacore/Uregex/view/PatterMatcherView04.java
+++ src/academy/devdojo/maratonajava/javacore/Uregex/view/PatterMatcherView04.java
@@ -1,9 +1,10 @@
 package academy.devdojo.maratonajava.javacore.Uregex.view;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatterMatcherView02 {
+public class PatterMatcherView04 {
     public static void main(String[] args) {
         // \d = Todos os digitos
         // \D = Tudo que não for digito
@@ -12,29 +13,26 @@ public class PatterMatcherView02 {
         // \w = a-Z A-Z, dígitos, _
         // \W = Tudo o que não fot incluso no \w
         // []
-        String regex = "[a-zA-C]";
-        String texto = "cafeBASE";
+        // ? zero ou uma
+        // * zero ou mais
+        // + uma ou mais
+        // {n, m} de n até m
+        // () agrupamento
+        // | ou
+        // $ fim da linha
+        // . 1.3 = 123, 133, 1@3, 1A3
+        String regex = "([a-zA-Z0-9._-])+@([a-zA-Z])+(\\.([a-z])+)+";
+        String texto = "luffy@hotmail.com, 123jotaro@gmail.com, #@!zoro@mail.br, teste@gmail.com.br, sakura@mail";
+
+        System.out.println("Email válido");
+        System.out.println("zoro@mail.br".matches(regex));
+        System.out.println(Arrays.toString(texto.split(",")));
+        System.out.println(texto.split(",")[1].trim());
+
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto);
 
         System.out.println("\nTexto:  " + texto);
-        System.out.println("Índice: 0123456789");
-        System.out.println("regex: " + regex);
-        System.out.println("Posições encontradas");
-        while (matcher.find()) {
-            System.out.print(matcher.start() + " " + matcher.group() + "\n");
-        }
-
-        int numeroHex = 0x59F86A;
-        System.out.println(numeroHex);
-
-        regex = "0[xX][0-9a-fA-F]";
-        String texto2 = "12 0x 0x 0xFFABC 0x109 0x1";
-
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(texto2);
-
-        System.out.println("\nTexto:  " + texto2);
         System.out.println("Índice: 0123456789");
         System.out.println("regex: " + regex);
         System.out.println("Posições encontradas");
