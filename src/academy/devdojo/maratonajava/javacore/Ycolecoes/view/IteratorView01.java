1d4bc1cc6566a591661813a9a5ac40ddb985e858
josevissoti <josevissoti@gmail.com>
2025-04-17 13:34:23 -0300

    Conversor List e Array, Iterator

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/view/IteratorView01.java
@@ -0,0 +1,33 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.view;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.domain.Manga;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorView01 {
+    public static void main(String[] args) {
+        List<Manga> mangas = new ArrayList<>();
+        mangas.add(new Manga(5L, "One Piece", 19.9, 0));
+        mangas.add(new Manga(1L, "Vagabond", 9.5, 5));
+        mangas.add(new Manga(4L, "Blue Lock", 3.2, 0));
+        mangas.add(new Manga(3L, "Pokemon", 11.2, 2));
+        mangas.add(new Manga(2L, "Hunter x Hunter", 2.9, 0));
+
+        Iterator<Manga> mangaIterator = mangas.iterator();
+
+        /*
+        while(mangaIterator.hasNext()){
+            Manga manga = mangaIterator.next();
+            if(manga.getQuantidade() == 0){
+                mangaIterator.remove();
+            }
+        }
+        */
+
+        mangas.removeIf(manga -> manga.getQuantidade() == 0);
+
+        System.out.println(mangas);
+    }
+}
