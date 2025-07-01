12fb9782f8291c9f44b0fd1368488ed86959bf78
josevissoti <josevissoti@gmail.com>
2025-07-01 13:22:41 -0300

    Wildcard

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/GenericsView01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.domain.Consumidor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsView01 {
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+        lista.add("Midoriya");
+        lista.add("Midoriya");
+
+        for (String o : lista) {
+            System.out.println(o);
+        }
+        addList(lista, new Consumidor("Midoriya"));
+
+    }
+
+    private static void addList(List lista, Consumidor consumidor){
+        lista.add((consumidor));
+    }
+}
