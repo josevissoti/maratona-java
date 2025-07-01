8b1dc51483a8de52c7c9406df0e1796fbb4733d1
josevissoti <josevissoti@gmail.com>
2025-07-01 18:51:47 -0300

    Generics

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/ClasseGenericaView02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.domain.Barco;
+import academy.devdojo.maratonajava.javacore.Zgenerics.service.BarcoRentavelService;
+
+public class ClasseGenericaView02 {
+
+    public static void main(String[] args) {
+        BarcoRentavelService barcoRentavelService = new BarcoRentavelService();
+        Barco barco = barcoRentavelService.buscarBarcoDisponivel();
+        System.out.println("Usando o barco por um mês...");
+        barcoRentavelService.retornarBarcoAlugado(barco);
+    }
+
+}
