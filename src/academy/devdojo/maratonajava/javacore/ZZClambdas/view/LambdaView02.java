6c36ed45154c80a9b8727f9fb6ba754f33016101
josevissoti <josevissoti@gmail.com>
2025-07-21 13:52:09 -0300

    Lambdas

---
+++ src/academy/devdojo/maratonajava/javacore/ZZClambdas/view/LambdaView02.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.view;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class LambdaView02 {
+    public static void main(String[] args) {
+        List<String> names = List.of("Natsu", "Alucard");
+        List<Integer> integers = map(names, s -> s.length());
+        List<String> strings = map(names, s -> s.toUpperCase());
+        System.out.println(integers);
+        System.out.println(strings);
+    }
+
+    private static <T, R> List<R> map(List<T> list, Function<T, R> function) {
+        List<R> result = new ArrayList<>();
+        for (T e : list) {
+            R r = function.apply(e);
+            result.add(r);
+        }
+        return result;
+    }
+}
